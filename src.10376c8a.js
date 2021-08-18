5cfffac8a3c72591fd56a507b8458e0ac81c1b09
krukroman <78154887+krukroman@users.noreply.github.com>
2021-08-18 20:41:31 +0000

    Deploying to gh-pages from @ krukroman/goit-js-hw-13-image-finder@0e25fcf4f7d2969eee067f7434de8e337c4608e4 🚀

--- src.10376c8a.js
+++ src.10376c8a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/main");
 },{"./sass/main.scss":"clu1","./js/main":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-13-image-finder/src.a91441c2.js.map
+//# sourceMappingURL=/goit-js-hw-13-image-finder/src.10376c8a.js.map
